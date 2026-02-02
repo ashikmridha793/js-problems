06e43155fec28d9daffbed11600ab402e4ded4ec
Ashik Mridha <ashikmridha43@gmail.com>
2026-02-02 23:38:15 +0600

    added tallest value

---
+++ tallest.js
@@ -0,0 +1,13 @@
+const numbers = [65, 66, 68, 72, 78, 60, 70];
+
+function getMax(numbers) {
+    let max = numbers[0];
+    for (const num of numbers) {
+        if (num > max) {
+            max = num;
+        }
+    }
+    return max
+}
+const max = getMax(numbers);
+console.log('max value is', max)
